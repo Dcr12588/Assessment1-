ee2df0b1dcfa905bdba30e4c97d818fb435bac75
Danny Ramirez <dcr1999jr@gmail.com>
2022-08-21 19:48:50 -0700

    Password File

--- password.js
+++ password.js
@@ -1,1 +1,13 @@
-let password = [`1`, `2`, `3`, `4`, `5`,`6`, `7` ,`8` ,`9` ,`A`]
+let password = `IchigoisGoat42`
+
+let minChars = false
+
+for (let i=0; i < password.length; i++){
+    console.log(password[i])
+}
+
+if (password.length >= 10){
+    minChars = true
+}
+
+console.log(minChars)
